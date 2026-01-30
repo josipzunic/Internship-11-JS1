2e6c22c92ce17698072b5555059dbad0ec7dbc42
josipzunic <jzunic123@gmail.com>
2026-01-30 12:56:38 +0100

    Add operation button functionality

--- script.js
+++ script.js
@@ -20,31 +20,108 @@ function subraction(x, y) {
     return x-y;
 }
 
-function findOperation(display, operations) {
-    const displayString = display.innerHTML;
-    let operationsArray = [];
-    for(let key in operations) operationsArray.push(operations[key]);
-    return operationsArray.find(operation => displayString.includes(operation));
+function multiplication(x, y) {
+    return x*y;
 }
 
-function equals(display, operations) {
-    const operation = findOperation(display, operations);
-    const displayString = display.innerHTML;
-    const numbersAsString = displayString.split(operation);
-    const numbers = numbersAsString.map(numberString => parseFloat(numberString));
+function division(x, y) {
+    if (y === 0) return "division by 0!";
+    return x/y;
+}
+
+function square(x) {
+    return x*x;
+}
+
+function squareRoot(x) {
+    if (x < 0) return "square root of negative number!";
+    return Math.sqrt(x);
+}
+
+function factorial(x) {
+    if (x < 0) return "factorial of negative number!";
+    const factors = [];
+    for(let i = x; i >= 1; i--) factors.push(i);
+    
+    const factorial = factors.reduce((factor, product) => product *= factor, 1);
+    return factorial;
+}
+
+function logarithm(x) {
+    if (x < 0) return "logarithm of negative number!";
+    return Math.log(x);
+}
+
+function findOperations(btnHTML, operations) {
+    let operationsArray = [];
+    for(let key in operations) operationsArray.push(operations[key]);
+    return operationsArray.filter(operation => 
+        btnHTML.includes(operation.replaceAll("x", "").replaceAll(")", "")));
+}
+
+function equals(display, operations, btnHTML) {
+    const operationsArray = findOperations(btnHTML, operations);
+    const operationsTrimmed = operationsArray.map(operation => 
+        operation.replaceAll("x", "").replaceAll(")", ""));
+    const displayString = display.innerHTML.trim();
+    const firstOperationTrimmed = operationsTrimmed[0];
+    const firstOperation = operationsArray[0];
+    const numbersAsString = displayString.split(firstOperationTrimmed).map(s => s.trim());
+    const numbers = numbersAsString
+        .map(numberString => parseFloat(numberString))
+        .filter(numberCandidat => !isNaN(numberCandidat));
 
     let result;
+    // console.log(operationsArray);
+    // console.log(displayString);
+    // console.log(numbersAsString);
+    // console.log(operationsTrimmed);
+    // console.log(firstOperation);
+    // console.log(numbers);
 
-    switch (operation) {
+    switch (firstOperation) {
         case calculatorOperationButtons.addition:
             result = addition(numbers[0], numbers[1]);
             break;
+
         case calculatorOperationButtons.subtract:
             result = subraction(numbers[0], numbers[1]);
             break;
-    }
 
-    console.log(result);
+        case calculatorOperationButtons.multiply:
+            result = multiplication(numbers[0], numbers[1]);
+            break;
+
+        case calculatorOperationButtons.division:
+            result = division(numbers[0], numbers[1]);
+            if (!isNaN(result)) {
+                result = Math.round(result*1000)/1000;
+            }
+            break;
+
+        case calculatorOperationButtons.square:
+            result = square(numbers[0]);
+            break;
+
+        case calculatorOperationButtons.sqaureRoot:
+            result = squareRoot(numbers[0]);
+            if (!isNaN(result)) {
+                result = Math.round(result*1000)/1000;
+            }
+            break;
+
+        case calculatorOperationButtons.factorial:
+            result = factorial(numbers[0]);
+            break;
+
+        case calculatorOperationButtons.logarithm:
+            result = logarithm(numbers[0]);
+            if (!isNaN(result)) {
+                result = Math.round(result*1000)/1000;
+            }
+            break;
+    }
+    display.innerHTML = `${result}`;
 }
 
 const calculatorOperationButtons = {
@@ -52,7 +129,7 @@ const calculatorOperationButtons = {
     shift: "shift",
     square: "x^2",
     sqaureRoot: "sqrt(x)",
-    multiply: "x",
+    multiply: "·",
     factorial: "!",
     addition: "+",
     logarithm: "log(x)",
@@ -216,21 +293,26 @@ const nonShiftOperations = [
     calculatorOperationButtons.square
 ];
 
-const nonShiftButtons = buttonsFromHTML.filter(btn => 
-    nonShiftOperations.includes(btn.innerHTML));
+const nonShiftButtons = buttonsFromHTML.filter(btn => {
+    return nonShiftOperations.includes(btn.innerHTML);
+});
+
+let btnHTML;
 
 nonShiftButtons.forEach(btn => {
     btn.addEventListener("click", () => {
+        let tempHTML = btn.innerHTML;
+
         if(btn.innerHTML.includes("x") || btn.innerHTML.includes(")")) {
-            btn.innerHTML = btn.innerHTML.replace("x", "");
-            btn.innerHTML = btn.innerHTML.replace(")", "");
+            tempHTML = tempHTML.replaceAll("x", "").replaceAll(")", "");
         }
-        writeToDisplay(btn.innerHTML, display);
+        writeToDisplay(tempHTML, display);
+        btnHTML = display.innerHTML;
     });
 });
 
 const equalsSign = buttonsFromHTML.find(btn => 
     btn.innerHTML === calculatorOperationButtons.equals);
 
-equalsSign.addEventListener("click", () => {equals(display, calculatorOperationButtons);});
+equalsSign.addEventListener("click", () => {equals(display, calculatorOperationButtons, btnHTML);});
 
